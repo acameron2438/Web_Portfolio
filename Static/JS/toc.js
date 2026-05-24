0bc64860cea9a14a510a55e03f38ed02cd8199eb
Andrew Cameron <acameron2438@gmail.com>
2026-05-24 00:48:14 -0400

    image upload 2

--- Static/JS/toc.js
+++ Static/JS/toc.js
@@ -1,4 +1,68 @@
 // ----------------------
+// Background Image
+// ----------------------
+// 1. Wrap your old logic in a function
+
+function pick_bg_image() {
+let backgrounds = [];
+const page = document.body.dataset.page;
+if (page === "bikes") {
+  backgrounds = [
+    '../Images/roman1.jpg',
+    '../Images/ren6.jpg'
+  ]
+  return backgrounds[Math.floor(Math.random() * backgrounds.length)];
+} else if (page === "welcome") {
+  backgrounds = [
+    "../Images/ren2.jpg",
+    "../Images/ren5.jpg"
+  ]
+  return backgrounds[Math.floor(Math.random() * backgrounds.length)];
+} else if (page === "salesforce") {
+  backgrounds = [
+    "../Images/ren3.jpg",
+    "../Images/ren1.jpg"
+  ]
+  return backgrounds[Math.floor(Math.random() * backgrounds.length)];
+} else if (page === "aipowered") {
+  backgrounds = [
+    "../Images/ren7.jpg",
+    "../Images/Granada.jpg"
+  ]
+  return backgrounds[Math.floor(Math.random() * backgrounds.length)];
+} else if (page === "howitworks") {
+  backgrounds = [
+    "../Images/greek1.jpg",
+    "../Images/ren4.jpg"
+  ]
+  return backgrounds[Math.floor(Math.random() * backgrounds.length)];
+  
+  } else {
+    return null;
+}
+}
+
+// 2. Call the function to get the chosen image
+const bgImage = pick_bg_image();
+
+if (bgImage) {
+//JavaScript filepath for both local testing and web (format: Static/Images/filename.jpg)
+const bgImage_js_step1=bgImage.slice(2)
+const bgImage_js_final='Static'+bgImage_js_step1
+
+// 3. Preload the image (this starts downloading immediately)
+const preload = new Image();
+preload.src = bgImage_js_final;
+// 4. When the image finishes downloading, apply it to CSS
+preload.onload = () => {
+  document.body.style.setProperty('--bg-image', `url(${bgImage})`);
+  document.body.classList.add('bg-loaded');
+};
+} else {
+  document.body.classList.add('bg-loaded');
+}
+
+// ----------------------
 // TOC Smooth Scroll
 // ----------------------
 document.querySelectorAll('.bike-toc a').forEach(link => {
@@ -44,9 +108,11 @@ const space = parseFloat(
 );
 document.querySelector('.page-content').style.height =
   `calc(100vh - ${navHeight}px - ${space}px)`;
+
 // ----------------------
 // Background Image
 // ----------------------
+/*
 const page = document.body.dataset.page;
 let backgrounds = [];
 if (page === "bikes") {
@@ -84,4 +150,4 @@ if (page === "bikes") {
   ]
   const chosen = backgrounds[Math.floor(Math.random() * backgrounds.length)];
   document.documentElement.style.setProperty("--bg-howitworks", `url(${chosen})`);
-}
+}*/
